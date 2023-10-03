126d688b44dae9583d9320df0dbfee1cb0f57234
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-03 23:14:22 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -18,9 +18,6 @@ define('LARAVEL_START', microtime(true));
 
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
-    ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 }
 
 /*
