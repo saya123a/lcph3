3825f1ef4e51c8247d12fc315b9554eb2fcdddd7
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-03 10:04:11 +0800

    Create AddnewproductController.php

---
+++ app/Http/Controllers/AddnewproductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AddnewproductController extends Controller
+{
+   public function addnewitem()
+   {
+       return view('addnewitem');
+   }
+}
