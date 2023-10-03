a10dd28fdc979d758d79852a8ba534625aa0fb47
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-03 10:06:26 +0800

    Update and rename Addnewproduct.php to Addnewitem.php

--- app/Models/Addnewitem.php
+++ app/Models/Addnewitem.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Addnewproduct extends Model
+class Addnewitem extends Model
 {
     use HasFactory;
 
