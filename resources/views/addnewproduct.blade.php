c6dc455c0121258e18e7b1e14cde17a5d8b0ad29
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-03 09:57:42 +0800

    Create addnewproduct.blade.php

---
+++ resources/views/addnewproduct.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Add New Stock</title>
+		<!--Meta Tag-->
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!--External CSS-->
+		<link rel="stylesheet" href="update.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+	</head>
+	<body>
+    <h1>Hai</h1>
+  </body>
+</html>
