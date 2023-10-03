b38562b440716affd2ef95874915fe50b9c7177f
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-03 10:04:06 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AddnewitemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/addnewitem', [AddnewitemController::class, 'addnewitem'])->name('addnewitem');
