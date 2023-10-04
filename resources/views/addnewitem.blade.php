e4cea20e1668a9b6373cebeae4977d4385e6545c
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-04 22:27:26 +0800

    Update addnewitem.blade.php

--- resources/views/addnewitem.blade.php
+++ resources/views/addnewitem.blade.php
@@ -8,6 +8,16 @@
 		<!--External CSS-->
 		<link rel="stylesheet" href="update.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $(".edit-button").click(function () {
+            const itemId = $(this).data("id");
+            $(`#edit-row-${itemId}`).toggle();
+        });
+    });
+</script>
+
 	</head>
 	<body>
     <h1>Hai</h1>
@@ -29,16 +39,36 @@
         </form>
         
         <!-- Display Data -->
-        <div>
-            <table>
-                @foreach($items as $item)
-                <tr>
-                    <td>{{ $item->item_barcode }}</td>
-                    <td>{{ $item->item_name }}</td>
-                    <td>{{ $item->item_brand }}</td>
-                </tr>
-                @endforeach
-            </table>
-        </div>
+<div>
+    <table>
+        @foreach($items as $item)
+        <tr>
+            <td>{{ $item->id }}</td>
+            <td>{{ $item->item_barcode }}</td>
+            <td>{{ $item->item_name }}</td>
+            <td>{{ $item->item_brand }}</td>
+            <td>
+                <button class="edit-button" data-id="{{ $item->id }}">Edit</button>
+            </td>
+        </tr>
+        <tr id="edit-row-{{ $item->id }}" style="display: none;">
+            <td colspan="7">
+                <!-- Edit Form (initially hidden) -->
+                <form method="post" action="{{ route('updateitem', ['id' => $item->id]) }}">
+                    @csrf
+                    @method('put')
+                    <div class="data-input">
+                        <label for="edit_item_barcode">Barcode:</label>
+                        <input type="text" id="edit_item_barcode" name="item_barcode" value="{{ $item->item_barcode }}" required>
+                    </div>
+                    <!-- Add similar fields for 'item_name' and 'item_brand' -->
+                    <button type="submit" name="submit" class="submit-button">Update</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+</div>
+
   </body>
 </html>
