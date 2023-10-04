1cc05f8b8f8d00e6d4348cf2c18d7783749730a0
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-04 16:29:44 +0800

    Update addnewitem.blade.php

--- resources/views/addnewitem.blade.php
+++ resources/views/addnewitem.blade.php
@@ -27,6 +27,13 @@
 			</div>
 			<button type="submit" name="submit" class="submit-button">Submit</button>
         </form>
-        
+        <table>
+        @foreach($show)
+        <td>{{$show->id}}</td>
+        <td>{{$show->item_barcode}}</td>
+        <td>{{$show->item_name}}</td>
+        <td>{{$show->item_brand}}</td>
+        @endforeach
+        </table>
   </body>
 </html>
