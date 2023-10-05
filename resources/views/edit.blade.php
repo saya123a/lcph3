c62c0953a6f2ed1d7ba826ea03e04d7cbe847229
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-05 09:19:42 +0800

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,49 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Add New Stock</title>
-		<!--Meta Tag-->
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<!--External CSS-->
-		<link rel="stylesheet" href="update.css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-	</head>
-	<body>
-    <h1>Hai22</h1>
-        <form method="post" action="{{route('addnewitems')}}">
+    <head>
+        <title>Edit Item</title>
+        <!--Meta Tag-->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--External CSS-->
+        <link rel="stylesheet" href="update.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    </head>
+    <body>
+        <h1>Edit Item</h1>
+        <form method="post" action="{{route('edit', ['item' => $item])}}">
             @csrf
             <div class="data-input">
-				<label for="item_barcode">Barcode:</label>
-				<input type="text" id="item_barcode" name="item_barcode" value="{{$item->item_barcode}}" required>
-			</div>
-			<div class="data-input">
-				<label for="item_name">Name:</label>
-				<input type="text" id="item_name" name="item_name" value="{{$item->item_name}}" required>
-			</div>
-			<div class="data-input">
-				<label for="item_brand">Brand:</label>
-				<input type="text" id="item_brand" name="item_brand" value="{{$item->item_brand}}" required>
-			</div>
-			<button type="submit" name="submit" class="submit-button">Update</button>
+                <label for="item_barcode">Barcode:</label>
+                <input type="text" id="item_barcode" name="item_barcode" value="{{$item->item_barcode}}" required>
+            </div>
+            <div class="data-input">
+                <label for="item_name">Name:</label>
+                <input type="text" id="item_name" name="item_name" value="{{$item->item_name}}" required>
+            </div>
+            <div class="data-input">
+                <label for="item_brand">Brand:</label>
+                <input type="text" id="item_brand" name="item_brand" value="{{$item->item_brand}}" required>
+            </div>
+            <button type="submit" name="submit" class="submit-button">Update</button>
         </form>
-        
-        <!-- Display Data -->
-<div>
-    <table>
-        @foreach($items as $item)
-        <tr>
-            <td>{{$item->id}}</td>
-            <td>{{$item->item_barcode}}</td>
-            <td>{{$item->item_name}}</td>
-            <td>{{$item->item_brand}}</td>
-            <td>
-                <a href="{{route('edit', ['item' => $item])}}">Edit</a>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-</div>
 
-  </body>
+        <!-- Display Data -->
+        <div>
+            <table>
+                @foreach($items as $item)
+                <tr>
+                    <td>{{$item->id}}</td>
+                    <td>{{$item->item_barcode}}</td>
+                    <td>{{$item->item_name}}</td>
+                    <td>{{$item->item_brand}}</td>
+                    <td>
+                        <a href="{{route('edit', ['item' => $item])}}">Edit</a>
+                    </td>
+                </tr>
+                @endforeach
+            </table>
+        </div>
+    </body>
 </html>
