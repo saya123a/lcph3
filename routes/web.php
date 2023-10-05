e88459b64d6357373cc27943b5a154a738aa7fc6
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-05 12:58:43 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,10 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');*/
 
 Route::get('/', [LoginController::class, 'login'])->name('lcph.login');
-
+/*
 Route::get('/addnewitem', [AddnewitemController::class, 'addnewitem'])->name('addnewitem');
 Route::post('/addnewitem', [AddnewitemController::class, 'addnewitems'])->name('addnewitems');
 Route::get('/addnewitem/{item}/edit', [AddnewitemController::class, 'edit'])->name('edit');
 Route::put('/addnewitem/{item}/update', [AddnewitemController::class, 'update'])->name('update');
 Route::delete('/addnewitem/{item}/delete', [AddnewitemController::class, 'delete'])->name('delete');
+*/
