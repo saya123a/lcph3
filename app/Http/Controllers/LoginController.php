1c9df9322e6c2b4c3bef64f2d42b709721a1f825
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-05 23:20:57 +0800

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -42,6 +42,6 @@ class LoginController extends Controller
 
         Login::create($data);
 
-        return redirect()->route('login')->with('msgsuccess', 'Successfully Registered.');
+        return redirect()->route('loginform')->with('msgsuccess', 'Successfully Registered.');
     }
 }
