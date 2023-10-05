77a75c73d37439d8189ed886cf933e300e7209e7
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-05 22:01:08 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,13 @@ use App\Http\Controllers\LoginController;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');*/
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/', [LoginController::class, 'login'])->name('login');
-/*
 Route::get('/addnewitem', [AddnewitemController::class, 'addnewitem'])->name('addnewitem');
 Route::post('/addnewitem', [AddnewitemController::class, 'addnewitems'])->name('addnewitems');
 Route::get('/addnewitem/{item}/edit', [AddnewitemController::class, 'edit'])->name('edit');
 Route::put('/addnewitem/{item}/update', [AddnewitemController::class, 'update'])->name('update');
 Route::delete('/addnewitem/{item}/delete', [AddnewitemController::class, 'delete'])->name('delete');
 */
+
+Route::get('/', [LoginController::class, 'login'])->name('login');
