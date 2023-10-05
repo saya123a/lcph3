036ef7152f133a99e9109493d14217d76a431641
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-06 00:15:18 +0800

    Update loginpage.blade.php

--- resources/views/loginpage.blade.php
+++ resources/views/loginpage.blade.php
@@ -41,6 +41,7 @@
 				<div class="form-inner">
 					<form method="post" action="{{route('login')}}" class="login">
                         @csrf
+                        @method('POST')
 						<div class="field">
 							<input type="text" name="username" placeholder="Username" required>
 						</div>
@@ -57,6 +58,7 @@
 					</form>
 					<form action="{{route('signup')}}" method="post" class="signup">
                         @csrf
+                        @method('POST')
 						<div class="field">
 							<input type="text" name="username" placeholder="Username" required>
 						</div>
