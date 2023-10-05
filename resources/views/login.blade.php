3c52620846a21a1ed4f1ec31feb47d250ec817b2
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-05 22:13:46 +0800

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,7 +6,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<!--External CSS-->
-		<link rel="stylesheet" href="update.css">
+		<link rel="stylesheet" href="login.css">
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 		<style>
 			html,body
