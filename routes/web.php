50b2aa4206be3c2cf89c182c7e712a015071cb03
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-05 12:45:42 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,15 @@ use App\Http\Controllers\AddnewitemController;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');*/
+
+Route::get('/', [LoginController::class, 'login'])->name('login');
 
 Route::get('/addnewitem', [AddnewitemController::class, 'addnewitem'])->name('addnewitem');
 Route::post('/addnewitem', [AddnewitemController::class, 'addnewitems'])->name('addnewitems');
