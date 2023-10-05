190fbd568d3f355c2eded689cc813ab9d322094c
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-05 13:01:01 +0800

    Update login.blade.php

--- resources/views/lcph/login.blade.php
+++ resources/views/lcph/login.blade.php
@@ -6,7 +6,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<!--External CSS-->
-		<link rel="stylesheet" href="{{asset('lcph/update.css') }}">
+		<link rel="stylesheet" href="{{asset('lcph/update.css')}}">
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 		<style>
 			html,body
