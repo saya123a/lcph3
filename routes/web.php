17016241ca0dc9314d7c29477dbd38d6ef8ed0c1
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-06 16:21:15 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AddnewitemController;
 use App\Http\Controllers\LoginpageController;
 use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\ResetPasswordController;
 
 /*
 |--------------------------------------------------------------------------
