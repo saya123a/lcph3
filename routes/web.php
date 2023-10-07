6fabecfe71ca17cc6a2a4188b8866d166899bdd3
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-07 09:32:34 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,17 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::middleware(['auth.check'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/addnewitem', [AddnewitemController::class, 'addnewitem'])->name('addnewitem');
+    Route::post('/addnewitem', [AddnewitemController::class, 'addnewitems'])->name('addnewitems');
+    Route::get('/addnewitem/{item}/edit', [AddnewitemController::class, 'edit'])->name('edit');
+    Route::put('/addnewitem/{item}/update', [AddnewitemController::class, 'update'])->name('update');
+    Route::delete('/addnewitem/{item}/delete', [AddnewitemController::class, 'delete'])->name('delete');
+    // Add more protected routes as needed
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/addnewitem', [AddnewitemController::class, 'addnewitem'])->name('addnewitem');
-Route::post('/addnewitem', [AddnewitemController::class, 'addnewitems'])->name('addnewitems');
-Route::get('/addnewitem/{item}/edit', [AddnewitemController::class, 'edit'])->name('edit');
-Route::put('/addnewitem/{item}/update', [AddnewitemController::class, 'update'])->name('update');
-Route::delete('/addnewitem/{item}/delete', [AddnewitemController::class, 'delete'])->name('delete');
 
 
 
