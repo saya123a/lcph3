54ad13e289246a377ea46c7f3a4c4787573ef740
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-07 21:40:42 +0800

    Update addnewitem.blade.php

--- resources/views/addnewitem.blade.php
+++ resources/views/addnewitem.blade.php
@@ -15,6 +15,7 @@
     margin-bottom: 20px; /* Add spacing between alerts */
     border-radius: 4px; /* Rounded corners for the alert */
     text-align: center;
+    width: 50%;
 }
 
 .alert-success {
