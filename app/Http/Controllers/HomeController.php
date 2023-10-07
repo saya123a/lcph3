cf07248b9918acb5a849f31fc6be102d626537ca
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-07 18:11:45 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,8 +18,14 @@ class HomeController extends Controller
         // Retrieve data from the "home" table using Eloquent
         $items = Home::all();
 
-        // Pass the retrieved data to the "home" view
-        return view('home', ['items' => $items]);
+        // Query to retrieve item_name, item_brand, and quantity for stock
+        $stockItems = DB::table('item')
+            ->select('item_name', 'item_brand')
+            ->selectRaw('COUNT(item_barcode) AS quantity')
+            ->groupBy('item_name', 'item_brand')
+            ->get();
+
+        return view('home', ['items' => $items, 'stockItems' => $stockItems]);
     }
 
     public function homes(Request $request)
@@ -55,17 +61,5 @@ class HomeController extends Controller
     {
         $item->delete();
         return redirect(route('home'));
-    }
-
-    public function stock()
-    {
-        // Query to retrieve item_name, item_brand, and quantity
-        $items = DB::table('items')
-            ->select('item_name', 'item_brand')
-            ->selectRaw('COUNT(item_barcode) AS quantity')
-            ->groupBy('item_name', 'item_brand')
-            ->get();
-
-        return view('home', ['items' => $items]);
     }
 }
