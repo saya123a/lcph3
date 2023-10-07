72dcdc0eda3745b363e48482c29c5cd505b687b3
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-07 22:52:09 +0800

    Update DeletecurrentitemController.php

--- app/Http/Controllers/DeletecurrentitemController.php
+++ app/Http/Controllers/DeletecurrentitemController.php
@@ -11,4 +11,22 @@ class DeletecurrentitemController extends Controller
     {
         return view('deletecurrentitem');
     }
+
+    public function deletecurrentitems(Request $request)
+    {
+        $itemBarcode = $request->input('item_barcode');
+
+        // Check if the barcode exists in the database
+        $existingItem = Item::where('item_barcode', $itemBarcode)->first();
+
+        if ($existingItem) {
+            // Barcode exists, delete the first matching record
+            Item::where('item_barcode', $itemBarcode)->delete();
+
+            return redirect()->back()->with('success', 'Data with barcode: ' . $itemBarcode . ' deleted successfully.');
+        } else {
+            // Barcode does not exist, show a notification
+            return redirect()->route('deletecurrentitem')->with('error', 'Barcode: ' . $itemBarcode . ' not found in the database.');
+        }
+    }
 }
