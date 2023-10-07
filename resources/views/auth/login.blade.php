471f63a3ba28a6fd11e333fea28e1c61a9bb709b
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-07 15:54:41 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,6 @@
 		    <link rel="stylesheet" href="{{ asset('login.css') }}">
 		    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <!-- Include Bootstrap CSS and JavaScript -->
-            <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
             <style>
