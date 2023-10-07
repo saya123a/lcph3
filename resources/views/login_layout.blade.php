d1a059365ecfe7b4ba13dbe74d6438701caa5f57
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-07 13:46:09 +0800

    Create login_layout.blade.php

---
+++ resources/views/login_layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Include CSS and other common meta tags, styles, and scripts here -->
+</head>
+<body>
+    @yield('content') <!-- This is where the login form content will be inserted -->
+    <!-- Include any additional scripts or footer content here -->
+</body>
+</html>
