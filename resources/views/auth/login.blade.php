3652243a3157d9555f4a9c5e8d7b93fdbc277ad3
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-07 10:43:44 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,7 +53,7 @@
                                 </span>
                             @enderror
                         </div>
-                        <div class="row mb-3">
+                        
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -62,7 +62,7 @@
                                     </label>
                                 </div>
                             </div>
-                        </div><br>
+                        
                         <div class="field btn">
 						    <div class="btn-layer"></div>
 							<input type="submit" name="login" type="button" value="Login">
