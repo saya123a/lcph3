70adf907bb8ddfd9a76bcb402a67abb4f6388124
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-07 22:11:34 +0800

    Update AddcurrentitemController.php

--- app/Http/Controllers/AddcurrentitemController.php
+++ app/Http/Controllers/AddcurrentitemController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Addcurrentitem;
 
-class AddcurentitemController extends Controller
+class AddcurrentitemController extends Controller
 {
     public function addcurrentitem()
     {
