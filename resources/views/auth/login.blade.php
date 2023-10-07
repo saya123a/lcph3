40764a30fb0305ffbe555b389421bdee2ce6d92e
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-07 15:52:49 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,6 +14,9 @@
         <!--External CSS-->
 		    <link rel="stylesheet" href="{{ asset('login.css') }}">
 		    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <!-- Include Bootstrap CSS and JavaScript -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 		    <style>
 		    	  html,body
 		    	  {
