099734b67347c5a93aa4e912930726fa9781c074
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-07 16:57:54 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Home;
 
 class HomeController extends Controller
 {
@@ -20,9 +21,52 @@ class HomeController extends Controller
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
-     */
+     
     public function index()
     {
         return view('home');
+    }*/
+    
+    public function home()
+    {
+        $items = Home::all();
+        return view('home', ['items' => $items]);
     }
+
+   public function homes(Request $request)
+   {
+       $data = $request->validate([
+           'item_barcode' => 'required',
+           'item_name' => 'required',
+           'item_brand' => 'required'
+       ]);
+
+       $add = Home::create($data);
+       return redirect(route('home'));
+   }
+
+   public function edit(Home $item)
+   {
+        return view('edit', ['item' => $item]);
+   }
+
+   public function update(Home $item, Request $request)
+   { 
+       $data = $request->validate([
+           'item_barcode' => 'required',
+           'item_name' => 'required',
+           'item_brand' => 'required'
+       ]);
+
+       $item->update($data);
+       return redirect(route('home'));
+   }
+
+   public function delete(Home $item)
+   {
+       $item->delete();
+       return redirect(route('home'));
+   }
+
+
 }
