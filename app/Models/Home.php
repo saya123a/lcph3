2a54fb58b2821a5299b1c114328ab43eecd5123d
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-07 16:55:35 +0800

    Create Home.php

---
+++ app/Models/Home.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Home extends Model
+{
+    use HasFactory;
+
+    protected $table = 'item';
+
+    protected $fillable = [
+      'item_barcode',
+      'item_name',
+      'item_brand'
+    ];
+}
