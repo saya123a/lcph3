0b00f80624c15b340d0b92248c563af20af6b345
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-08 16:02:56 +0800

    Update CheckoutController.php

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Checkout;
+use App\Models\Receiver;
 
 class CheckoutController extends Controller
 {
     public function checkout()
     {
-        return view('checkout');
+        $receivers = Receiver::all(); // Retrieve all receiver details from the database
+
+        return view('checkout', compact('receivers'));
     }
 }
