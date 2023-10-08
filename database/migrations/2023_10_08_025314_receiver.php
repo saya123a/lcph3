a098b066602d1d5d35e0b146788d339296873ef3
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-08 11:05:45 +0800

    Update 2023_10_08_025314_receiver.php

--- database/migrations/2023_10_08_025314_receiver.php
+++ database/migrations/2023_10_08_025314_receiver.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('receiver', function (Blueprint $table) {
             $table->id();
-            $table->int('ic');
+            $table->integer('ic');
             $table->string('name');
             $table->timestamps();
         });
