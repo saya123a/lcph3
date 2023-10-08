c02b7c1dc71e57bc2d65905c778ef231c4edcb70
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-08 16:26:38 +0800

    Create ItemReceiver.php

---
+++ app/Models/ItemReceiver.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ItemReceiver extends Model
+{
+    use HasFactory;
+
+    protected $table = 'item_receiver';
+
+    protected $fillable = [
+      'item_barcode',
+      'item_name',
+      'item_brand',
+      'receiver_ic',
+      'receiver_name'
+    ];
+
+    // Define the relationship with the 'items' table
+    public function item()
+    {
+        return $this->belongsTo(Item::class, 'item_barcode');
+    }
+
+    // Define the relationship with the 'receiver' table
+    public function receiver()
+    {
+        return $this->belongsTo(Receiver::class, 'receiver_ic');
+    }
+}
