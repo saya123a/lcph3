7b7d1dfa0803a327f15ebd51878d7d381b19dbcf
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-08 17:08:01 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ReceiverController;
 use App\Http\Controllers\AddnewitemController;
 use App\Http\Controllers\AddcurrentitemController;
 use App\Http\Controllers\DeletecurrentitemController;
@@ -28,10 +29,13 @@ Auth::routes();
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/home', [HomeController::class, 'home'])->name('home');
-    Route::post('/home', [HomeController::class, 'homes'])->name('homes');
+    Route::get('/home', [ReceiverController::class, 'getreceivers'])->name('getreceivers');;
+
+    /**Route::post('/home', [HomeController::class, 'homes'])->name('homes');
     Route::get('/home/{item}/edit', [HomeController::class, 'edit'])->name('edit');
     Route::put('/home/{item}/update', [HomeController::class, 'update'])->name('update');
-    Route::delete('/home/{item}/delete', [HomeController::class, 'delete'])->name('delete');
+    Route::delete('/home/{item}/delete', [HomeController::class, 'delete'])->name('delete');**/
+    
     Route::get('/addnewitem', [AddnewitemController::class, 'addnewitem'])->name('addnewitem');
     Route::post('/addnewitem', [AddnewitemController::class, 'addnewitems'])->name('addnewitems');
     Route::get('/addcurrentitem', [AddcurrentitemController::class, 'addcurrentitem'])->name('addcurrentitem');
@@ -43,7 +47,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/deletecurrentreceiver', [DeletecurrentreceiverController::class, 'deletecurrentreceiver'])->name('deletecurrentreceiver');
     Route::post('/deletecurrentreceiver', [DeletecurrentreceiverController::class, 'deletecurrentreceivers'])->name('deletecurrentreceivers');
     Route::get('/checkout', [CheckoutController::class, 'checkout'])->name('checkout');
-    Route::post('/checkout', [CheckoutController::class, 'checkouts'])->name('checkouts'); 
+    Route::post('/checkout', [CheckoutController::class, 'checkouts'])->name('checkouts');
 
     // Add more protected routes as needed
 });
