66554b7ddeb4f8122e2762b2838603b7f7ad8076
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-08 12:34:05 +0800

    Create Deletecurrentreceiver.php

---
+++ app/Models/Deletecurrentreceiver.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Addnewreceiver extends Model
+{
+    use HasFactory;
+
+    protected $table = 'receiver';
+
+    protected $fillable = [
+      'receiver_ic',
+      'receiver_name'
+    ];
+}
