bc6c38c76b9f836540009dd6f27a58c2704aafa2
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-08 16:34:51 +0800

    Update checkout.blade.php

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -90,6 +90,17 @@
     </div>
     <button type="submit" name="submit" class="submit-button">Submit</button>
 </form>
+                @if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
+@if(session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
 			</section>
 		</main>
 		<div class="nutri">
