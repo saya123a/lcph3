9e53997baee81241c4c15ea00f293ea6794bba7d
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-09 21:48:54 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,7 +49,7 @@
 					</li>
 					<li><a href="{{ route('checkout') }}">Checkout</a></li>
                     <li>
-                        <div class='dropdown'>
+                        <!--<div class='dropdown'>
                             <a button  class="nav-link dropdown-toggle" role="button" id="navbarDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
                             </button></a>
@@ -63,7 +63,22 @@
                                     @csrf
                                 </form>
 	                        </div>
-                        </div>
+                        </div>-->
+                        <div class="dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-bs-toggle="dropdown">
+        {{ Auth::user()->name }}
+    </a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="{{ route('logout') }}"
+           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+    </div>
+</div>
+
                     </li>
 				</ul>
 				<div class="icon menu-btn">
