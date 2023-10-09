baca4b96d1436a07c086056bae1961e3cd495bfc
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-09 12:34:18 +0800

    Update ReceiverController.php

--- app/Http/Controllers/ReceiverController.php
+++ app/Http/Controllers/ReceiverController.php
@@ -43,4 +43,29 @@ class ReceiverController extends Controller
             return redirect(route('addnewreceiver'))->with('success', 'Receiver with IC: ' . $data['receiver_ic'] . ' inserted successfully!');
         }
     }
+
+    public function deletecurrentreceiver()
+    {
+        return view('deletecurrentreceiver');
+    }
+
+    public function deletecurrentreceivers(Request $request)
+    {
+        $receiverIc = $request->input('receiver_ic');
+
+        // Find the first matching item
+        $existingReceiver = Receiver::where('receiver_ic', $receiverIc)->first();
+
+        if ($existingReceiver) {
+            // Receiver exists, delete the first matching record
+            $existingReceiver->delete();
+
+            return redirect()->back()->with('success', 'Receiver with IC: ' . $receiverIc . ' deleted successfully.');
+        } else {
+            // Barcode does not exist, show a notification
+            return redirect()->route('deletecurrentreceiver')->with('error', 'IC: ' . $receiverIc . ' does not exist.');
+        }
+
+        return redirect()->route('deletecurrentreceiver')->with('message', $message);
+    }
 }
