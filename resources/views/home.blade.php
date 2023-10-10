e5ee9a6fef538357b8110162f0b37a766ef410b5
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-10 12:01:21 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<!--External CSS-->
-		<link rel="stylesheet" href="update.css">
+		<link rel="stylesheet" href="updates.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 	</head>
 	<body>
