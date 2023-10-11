4f405857c77bb51d2c97b97294f925ef7b4cb4f3
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-11 22:21:46 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,12 @@ use App\Http\Controllers\CheckoutController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
- 
+
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,6 +50,35 @@ Route::post('/forgot-password', function (Request $request) {
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/home', [HomeController::class, 'home'])->name('home');
+
+Route::get('/reset-password/{token}', function ($token) {
+    return view('auth.reset-password', ['token' => $token]);
+})->middleware('guest')->name('password.reset');
+ 
+Route::post('/reset-password', function (Request $request) {
+    $request->validate([
+        'token' => 'required',
+        'email' => 'required|email',
+        'password' => 'required|min:8|confirmed',
+    ]);
+ 
+    $status = Password::reset(
+        $request->only('email', 'password', 'password_confirmation', 'token'),
+        function ($user, $password) {
+            $user->forceFill([
+                'password' => Hash::make($password)
+            ])->setRememberToken(Str::random(60));
+ 
+            $user->save();
+ 
+            event(new PasswordReset($user));
+        }
+    );
+ 
+    return $status === Password::PASSWORD_RESET
+                ? redirect()->route('login')->with('status', __($status))
+                : back()->withErrors(['email' => [__($status)]]);
+})->middleware('guest')->name('password.update');
     
     /**Route::post('/home', [HomeController::class, 'homes'])->name('homes');
     Route::get('/home/{item}/edit', [HomeController::class, 'edit'])->name('edit');
