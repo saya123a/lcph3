3f7cb2ffe304615f8a9e2155d7d4d7dff486ccc1
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-11 22:57:47 +0800

    Update ResetPasswordController.php

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -6,31 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
-// Import the required classes at the top of the file
-use App\Notifications\CustomResetPasswordNotification;
-
-class ResetPasswordController extends Controller
-{
-    use ResetsPasswords;
-
-    // Override the sendResetLinkEmail method
-    protected function sendResetLinkEmail(Request $request)
-    {
-        $this->validateEmail($request);
-
-        // We will send the password reset link to this user.
-        $user = $this->broker()->getUser($this->credentials($request));
-
-        if ($user && ! $user->isPasswordResetDisabled()) {
-            $user->sendPasswordResetNotification(
-                $this->broker()->createToken($user)
-            );
-        }
-
-        return $this->sendResetLinkResponse($request, Password::RESET_LINK_SENT);
-    }
-}
-/**
 class ResetPasswordController extends Controller
 {
     /*
@@ -42,7 +17,7 @@ class ResetPasswordController extends Controller
     | and uses a simple trait to include this behavior. You're free to
     | explore this trait and override any methods you wish to tweak.
     |
-    *
+    */
 
     use ResetsPasswords;
 
@@ -50,6 +25,6 @@ class ResetPasswordController extends Controller
      * Where to redirect users after resetting their password.
      *
      * @var string
-     *
+     */
     protected $redirectTo = RouteServiceProvider::HOME;
-}**/
+}
