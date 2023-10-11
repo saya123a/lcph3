b7859a3aabe52980ed485d5a8ea6c513c07dc56d
saya123a <116484495+saya123a@users.noreply.github.com>
2023-10-11 22:36:20 +0800

    Update MyEmail.php

--- app/Mail/MyEmail.php
+++ app/Mail/MyEmail.php
@@ -5,8 +5,6 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class MyEmail extends Mailable
@@ -22,32 +20,12 @@ class MyEmail extends Mailable
     }
 
     /**
-     * Get the message envelope.
+     * Build the message.
      */
-    public function envelope(): Envelope
+    public function build()
     {
-        return new Envelope(
-            subject: 'My Email',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this
+            ->subject('My Email Subject')
+            ->view('emails.my_email'); // Blade view for email content
     }
 }
